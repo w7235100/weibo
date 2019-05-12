8e7965b83f0180e5c98420f5c5866109d3eab34c
Weijiaxian <1121414656@qq.com>
2019-05-12 13:07:53 +0000

    用户模拟数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\User::class,50)->create();
+        $user=\App\Models\User::find(1);
+        $user->name='admin';
+        $user->email='admin@qq.com';
+        $user->password='admin';
+        $user->save();
+    }
+}
